dc2ce28f8b584f0285b97c4c6f68d66112663ba4
Pwl Webdev <pwl.webdev@gmail.com>
2016-08-10 20:04:28 +0200

    Comment deletion completed

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,20 +1,31 @@
 class CommentsController < ApplicationController
 	before_action :authenticate_user!
+	before_action :correct_user,   only: :destroy
+
 	def new
 		@comment = Comment.new
 	end
 
 	def create
 		@comment = current_user.comments.build(comment_params)
-		#@comment = Comment.new(:user => current_user, :post_id => comment_params[:post_id], :content => comment_params[:content])
 		if @comment.save!
 			flash[:success] = "Comment created!"
 		end
 		redirect_to user_path(current_user)
 	end
 
+	def destroy
+		@comment.destroy
+		flash[:success] = "Comment deleted!"
+		redirect_to user_path(current_user)
+	end
+
 	private
 	def comment_params
 		params.require(:comment).permit(:content, :post_id)
+	end
+	def correct_user
+		@comment = current_user.comments.find_by(id: params[:id])
+		redirect_to root_url if @comment.nil?
 	end
 end
